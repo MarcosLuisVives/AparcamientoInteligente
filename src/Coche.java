4509b6578462af0afedf813942a7a6e5c49871ab
Marcos Contreras <marcos.contreras@alumno.iesluisvives.org>
2025-10-20 16:12:51 +0200

    cambio metodo toString clase coche y correcion metodo entrar

--- src/Coche.java
+++ src/Coche.java
@@ -1,46 +1,39 @@
 import java.util.Random;
+
 public class Coche extends Thread {
     private String nombre;
     private boolean vip;
     private Estacionamiento aparcamiento;
     private final Random random = new Random();
 
-  public Coche(String nombre, Estacionamiento aparcamiento, boolean vip) {
-    this.nombre = nombre;
-    this.aparcamiento = aparcamiento;
-    this.vip =vip;
-  }
-   public boolean esVip(){
-      return vip;
-   }
+    public Coche(String nombre, Estacionamiento aparcamiento, boolean vip) {
+        this.nombre = nombre;
+        this.aparcamiento = aparcamiento;
+        this.vip = vip;
+    }
+
+    public boolean esVip() {
+        return vip;
+    }
 
     @Override
     public void run() {
-        // Intentar aparcar
         if (!aparcamiento.entrar(this)) {
-            System.out.println( nombre + " se marcha, no pudo entrar.");
+            System.out.println(nombre + " se marcha, no pudo entrar.");
             return;
         }
-
-        // Simular el tiempo estacionado (2–6 segundos)
-        int tiempo = random.nextInt(5) + 2;
+        int tiempo = random.nextInt(5) + 10;
         System.out.println(nombre + " está estacionado (" + tiempo + "s).");
-
         try {
             Thread.sleep(tiempo * 1000L);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-
-        // Salir del aparcamiento
         aparcamiento.salir(this);
     }
+
     @Override
-    public String toString(){
-        if(vip){
-            return "Coche VIP: " + nombre;
-        }else{
-            return "Coche Normal: " + nombre;
-        }
+    public String toString() {
+        return vip ? nombre + "[VIP]" : nombre + "[Normal]";
     }
 }
--- src/Estacionamiento.java
+++ src/Estacionamiento.java
@@ -4,24 +4,22 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Estacionamiento {
-    private final AtomicInteger capacidadMaxima=new AtomicInteger(5);
-    private final Semaphore semaforo=new Semaphore(5);
-    private final ArrayList<Coche> coches=new ArrayList<>();
+    private final AtomicInteger capacidadMaxima = new AtomicInteger(5);
+    private final Semaphore semaforo = new Semaphore(5);
+    private final ArrayList<Coche> coches = new ArrayList<>();
 
     public synchronized boolean entrar(Coche coche) {
         try {
-            // Intentar entrar normalmente
             if (semaforo.tryAcquire(5, TimeUnit.SECONDS)) {
                 coches.add(coche);
                 System.out.println(coche + " ha entrado. Plazas libres: " + semaforo.availablePermits());
                 return true;
             } else {
-                // No hay hueco
                 if (coche.esVip()) {
-                    // Forzar entrada VIP desalojando un coche normal
                     desalojarCocheNormal(coche);
                     System.out.println(coche + " (VIP) ha entrado desalojando a un coche normal.");
                     return true;
+
                 } else {
                     System.out.println(coche + " no pudo entrar (parking lleno).");
                     return false;
@@ -33,22 +31,22 @@ public class Estacionamiento {
         }
     }
 
-    public void salir(Coche coche) {
-        coches.remove(coche);
-        semaforo.release();
-        System.out.println(coche + " ha salido. Plazas libres: " + semaforo.availablePermits());
+    public synchronized void salir(Coche coche) {
+        if (coches.remove(coche)) {
+            semaforo.release();
+            System.out.println(coche + " ha salido. Plazas libres: " + semaforo.availablePermits());
+        }
     }
-    public void desalojarCocheNormal(Coche cocheVip) {
-        for(Coche coche:coches){
-            if(!coche.esVip()){
-                salir(coche);
+
+    public synchronized void desalojarCocheNormal(Coche cocheVip) {
+        for (Coche coche : new ArrayList<>(coches)) {
+            if (!coche.esVip()) {
+                coches.remove(coche);
+                System.out.println(coche + " fue desalojado por " + cocheVip);
                 coches.add(cocheVip);
 
             }
         }
+
     }
-
-
-
-
 }
